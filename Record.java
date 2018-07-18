3b9e2f2fd83ed5cb64cb478da5f25cc51a567eca
PTAdrian1997 <poptifreaadrian@gmail.com>
2018-07-18 13:11:24 +0300

    Record.java file added

---
+++ Record.java
@@ -0,0 +1,22 @@
+public class Record {
+
+    private String name;
+    private String id;
+
+    public void setName(String newName){
+        this.name = newName;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public String getId(){
+        return this.id;
+    }
+
+}
